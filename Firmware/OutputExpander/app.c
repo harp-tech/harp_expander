a7ae129d870128ed111b82820d658f3bc2013dc1
Filipe Carvalho <34546039+filcarv@users.noreply.github.com>
2021-02-23 18:58:53 +0000

    Added default for the magnetic encoder sample rate.

--- Firmware/OutputExpander/app.c
+++ Firmware/OutputExpander/app.c
@@ -120,6 +120,8 @@ void core_callback_reset_registers(void)
 	app_regs.REG_SERVO0_PULSE_US = 1500;	// 1.5 ms
 	app_regs.REG_SERVO1_PULSE_US = 1500;	// 1.5 ms
 	app_regs.REG_SERVO2_PULSE_US = 1500;	// 1.5 ms
+	
+	app_regs.REG_MAG_ENCODER_MODE = MSK_AT_100SPS;
 }
 
 void core_callback_registers_were_reinitialized(void)
